140d06453833343d47c728d3a8713bb864e50a74
trickypr <git@trickypr.com>
2025-01-26 22:33:17 +1100

    fix: package bundled js

--- server/package.nix
+++ server/package.nix
@@ -7,9 +7,9 @@ pkgs.buildGleamApplication {
   rebar3Package = pkgs.rebar3WithPlugins { plugins = with pkgs.beamPackages; [ pc ]; };
 
   postConfigure = ''
-    mkdir -p priv/bundled
+    mkdir -p priv
     cp -r ${web}/css ./priv
-    cp -r ${web}/out/js/* ./priv/bundled
+    cp -r ${web}/out/bundled ./priv
     touch priv/is-nix-pkg
   '';
 }
